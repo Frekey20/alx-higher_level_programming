71dabe83bd435e0990ac74ea827f96e4c1dbd74f
frekey20 <frekeycolz@gmail.com>
2023-07-22 17:10:46 -0700

    updated

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-
-    try:
-        print("{:d}".format(value))
-        return True
-    except ValueError:
-        print("it is not an integer")
-	return False
+	try:
+		print("{:d}".format(value))
+	except (ValueError, TypeError):
+		print("it is not an integer")
+		return False
+	return True
